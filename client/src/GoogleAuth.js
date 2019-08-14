11b614e8fb0cd6c32253f8848577b56a62a7589d
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-08-14 16:52:40 -0700

    Implementing Auth Reducer

--- client/src/GoogleAuth.js
+++ client/src/GoogleAuth.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "./actions";
 
-class GoogleAauth extends React.Component {
+class GoogleAuth extends React.Component {
   state = { isSignedIn: null };
   componentDidMount() {
     window.gapi.load("client:auth2", () => {
+      /* This is a call back method saying gapi.client.init and telling client id and what we want to modify */
       window.gapi.client
         .init({
           clientId:
@@ -11,18 +14,46 @@ class GoogleAauth extends React.Component {
           scope: "email"
         })
         .then(() => {
-          this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+          this.auth = window.gapi.auth2.getAuthInstance(); // getting the instance of the auth.
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() }); // checking the status if we are signed in or not and then updating the state for the screen to re render
+          this.auth.isSignedIn.listen(this.onAuthChange());
         });
     });
   }
+  // since this is a call back function using arrow funtion so the context is bound to the component.
+  onAuthChange = isSignedIn => {
+    if (isSignedIn) {
+      this.props.signIn();
+    } else {
+      this.props.signOut();
+    }
+  };
+
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn == null) {
       return <div>I dont know if we are signed in</div>;
     } else if (this.state.isSignedIn) {
-      return <div>we are signed in</div>;
+      return (
+        <button onClick={this.onSignOutClick} className="ui red google button">
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
     } else {
-      return <div> we are not signed in</div>;
+      return (
+        <button onClick={this.onSignInClick} className="ui red google button">
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      );
     }
   }
   render() {
@@ -30,4 +61,7 @@ class GoogleAauth extends React.Component {
   }
 }
 
-export default GoogleAauth;
+export default connect(
+  null,
+  { signIn, signOut }
+)(GoogleAuth);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import GoogleAuth from "../GoogleAuth";
+
 const Header = () => {
   return (
     <div className="ui secondary pointing menu">

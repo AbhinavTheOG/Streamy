f19c42c0f873dd28184193ec2c3f98434875f9fc
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-08-15 11:50:08 -0700

    Wired redux form

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import { reducer as formReducer } from "redux-form"; // getting the in built reducer and naming it as formReducer
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  form: formReducer
 });

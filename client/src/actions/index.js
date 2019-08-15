dabf33fb920c11fc10669d19621d8c07db4ec297
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-08-15 14:49:43 -0700

    Creating a stream with Rest Conventions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from "../apis/streams";
+
 export const signIn = userId => {
   return {
     type: "SIGN_IN",
@@ -9,4 +11,8 @@ export const signOut = () => {
   return {
     type: "SIGN_OUT"
   };
+};
+
+export const createStream = formValues => async dispatch => {
+  streams.post("./streams", formValues);
 };
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
-
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
     if (touched && error) {
@@ -33,9 +34,9 @@ class StreamCreate extends React.Component {
   };
 
   //handle submit does the prevent default automaticaly passes the event
-  onSubmit(formValues) {
-    console.log(formValues);
-  }
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
 
   render() {
     return (
@@ -68,7 +69,18 @@ const validate = formValues => {
   return errors;
 };
 
-export default reduxForm({
+const formWraped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(
+  null,
+  { createStream }
+)(formWraped);
+
+// we can use this too.
+// export default connect() (reduxForm({
+//   form: "streamCreate",
+//   validate
+// })(StreamCreate));

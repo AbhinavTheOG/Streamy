eff3d565f9041ddca5029c366ff55fdd1c0e24af
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-08-16 16:45:34 -0700

    Assosiating Streams with Users

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui classed list">{this.renderList()}</div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) }; // built in js function that pulls out all the things in an onject and puts them in an array.
 };
-
-export default StreamList;
+export default connect(
+  mapStateToProps,
+  { fetchStreams }
+)(StreamList);

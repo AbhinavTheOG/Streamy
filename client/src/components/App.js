967176eab2b6fa89f30c5fb9c30216e9bd0a7915
AbhinavTheOG <48139542+AbhinavTheOG@users.noreply.github.com>
2019-08-20 09:01:07 -0700

    Basic CRUD application done

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Switch, Router, Route } from "react-router-dom";
 
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -8,17 +8,20 @@ import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
 import history from "../history";
 import Header from "./Header";
+
 const App = () => {
   return (
     <div className="ui container">
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/Streams/new" exact component={StreamCreate} />
-          <Route path="/Streams/edit/:id" exact component={StreamEdit} />
-          <Route path="/Streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/Streams/show" exact component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/Streams/new" exact component={StreamCreate} />
+            <Route path="/Streams/edit/:id" exact component={StreamEdit} />
+            <Route path="/Streams/delete/:id" exact component={StreamDelete} />
+            <Route path="/Streams/:id" exact component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
